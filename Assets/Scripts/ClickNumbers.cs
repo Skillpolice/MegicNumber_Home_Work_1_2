7bce1f0cfb16ddaf2251d3b376f69d0a44a41a9a
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2020-09-25 20:47:19 +0300

    Initial commit

---
+++ Assets/Scripts/ClickNumbers.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickNumbers : MonoBehaviour
+{
+    public Text title;
+
+    int sum;
+    int allSum = 50;
+    bool stopNum = false;
+    int ct;
+
+    void Start()
+    {
+        title.text = "Нажмите клавижу от 1 до 9";
+    }
+
+
+    void Update()
+    {
+        if(sum <50)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                sum += 1;
+                ct += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                sum += 2;
+                ct += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                sum += 3;
+                ct += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad4))
+            {
+                sum += 4;
+                ct += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad5))
+            {
+                sum += 5;
+                ct += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad6))
+            {
+                sum += 6;
+                ct += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad7))
+            {
+                sum += 7;
+                ct += 1;
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Keypad8))
+            {
+                sum += 8;
+                ct++;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad9))
+            {
+                sum += 9;
+                ct += 1;
+            }    
+        }
+        else
+        {
+            GetSum();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ct = 0;
+            sum = 0;
+            Start();
+        }
+
+    }
+
+
+
+    public void GetSum()
+    {
+
+        title.text = "Summa = " + sum.ToString() + "\n" + " Кол-во проходов в нoги)) " + ct;
+        if (sum >= allSum)
+        {
+            title.text = "Game Over" + "\n" +  "Summa =" + sum + "\n" + " Кол-во проходов в нoги)) " + ct;
+        }
+
+    }
+}
---
+++ Assets/Scripts/MagicNum.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MagicNum : MonoBehaviour
+{
+    public Text title;
+
+    public int min;
+    public int max;
+    int guess;
+
+    int ct = 0;
+
+    void Start()
+    {
+        print("Закадайте число от 1 до " + max);
+        UpdateGuess();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //нажатие клавишы вверх и вычитание среднеарефметичесткого
+        {
+            min = guess;
+            UpdateGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) //нажатие клавишы вниз и вычитание среднеарефметичесткого
+        {
+
+            max = guess;
+            UpdateGuess();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            title.text = "Поздравляем это ваше число " + guess.ToString() + "\n" + "Кол-во потраченых шагов " + ct;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ct = 0;
+            min = 1;
+            max = 1000;
+            Start();
+            print("----------------NEW GAME-------------------");
+        }
+
+    }
+
+
+    void UpdateGuess()
+    {
+        ct += 1;
+        guess = (min + max) / 2;
+        title.text = "ваше число " + guess.ToString() + " ?";
+
+    }
+
+}
